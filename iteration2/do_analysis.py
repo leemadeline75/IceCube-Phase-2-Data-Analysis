2acda155b4b707391f0e1ab4159450ead0054f90
leemadeline75 <leemadeline75@gmail.com>
2025-06-02 12:15:50 -0400

    Update do_analysis.py

--- iteration2/do_analysis.py
+++ iteration2/do_analysis.py
@@ -3,7 +3,7 @@ import os
 import argparse
 import sys
 from reducer import Reducer
-from phase2_data_analysis import consolidateData, makePlots, plot_stacked_histogram, plot_correct_fraction_histogram, plotuserenergy_2d, plotDNNenergy_2d, plot_accuracy_colored_by_confidence, scatterplot_iter2, plot_accuracy_heatmaps, userDNNaccuracy, accuracybycat_subset
+from phase2_data_analysis_2 import consolidateData, makePlots
 from get_retired import getRetired
 from energy_histo_plot import qtot2d_hist
 
@@ -72,32 +72,5 @@ if __name__ == '__main__':
         
         print('Complete!')
 
-        qtot = result_consensus['qtot'] #make histogram w accuracy
-        qsig = result_consensus['signal_charge']
-        qbg = result_consensus['bg_charge']
-        accuracy = result_consensus['user_accuracy']
-        energy = result_consensus['energy']
-        userconf = result_consensus['data.agreement']
-        DNNconfidence = result_consensus['max_score_val']
-        DNNaccuracy = result_consensus['DNN_accuracy']
-        ntn_category = result_consensus['ntn_category']
-        #qratio = result_consensus['qratio']
-        #plot_correct_fraction_histogram(qtot, accuracy)
-        #qtot2d_hist(qtot, energy, accuracy, userconf)
-        #plotuserenergy_2d(energy,userconf)
-        #plotDNNenergy_2d(energy, DNNconfidence)
-        #plot_accuracy_colored_by_confidence(qtot, accuracy, userconf)
-        #scatterplot_iter2(qtot, energy, accuracy, userconf, qratio)
-        #plot_accuracy_heatmaps(qtot, DNNaccuracy, DNNconfidence)
-        #userDNNaccuracy(accuracy, DNNaccuracy, qtot)
-        #accuracybycat1(accuracy,qtot,ntn_category, DNNaccuracy, num_bins=10)
-#cascade bin size was 65 to 4250 0.165 
-        bin_config = {
-            'CASCADE': {'range': (65, 8500), 'bin_width': 0.195},
-            'SKIMMING': {'range': (85, 415), 'bin_width': 0.065},
-            'THROUGHGOINGTRACK': {'range': (95, 310), 'bin_width': 0.065},
-            'STARTINGTRACK': {'range': (70, 500), 'bin_width': 0.13},
-            'STOPPINGTRACK': {'range': (85, 180), 'bin_width': 0.065},}
-        accuracybycat_subset(accuracy=accuracy,qtot=qtot, ntn_category=ntn_category, DNNaccuracy=DNNaccuracy, bin_config=bin_config)
 
 
